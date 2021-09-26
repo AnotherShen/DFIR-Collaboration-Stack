cfbfd9288b0c7e5c0f8579c84442a4291a64c48e
AnotherShen <9160174+AnotherShen@users.noreply.github.com>
2021-09-26 06:50:25 -0700

    Update install.sh

--- services/onlyoffice/install.sh
+++ services/onlyoffice/install.sh
@@ -1,13 +1,16 @@
 !#/bin/bash
 
-# Step 1 - Generate Certs for 365 days
+# Step 1 - Generate passwords using pwgen
+
+# Step 2 - Generate Certs for 365 days
+[[ -f certs ]] && rm -rf certs
+mkdir certs
 cd certs
-rm *
 openssl genrsa -out onlyoffice.key 2048
 openssl req -new -key onlyoffice.key -out onlyoffice.csr
 openssl x509 -req -days 365 -in onlyoffice.csr -signkey onlyoffice.key -out onlyoffice.crt
 openssl dhparam -out dhparam.pem 2048
 cd ..
 
-# Step 2 - Bring service up
+# Step 3 - Bring service up
 sudo docker-compose up -d
