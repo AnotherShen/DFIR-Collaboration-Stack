59da6c922a9eaa86acfb36c968bee4115d8a0574
AnotherShen <9160174+AnotherShen@users.noreply.github.com>
2021-11-01 05:55:22 +0000

    Added some cleanup steps

--- reset_docker.sh
+++ reset_docker.sh
@@ -4,7 +4,14 @@
 sudo docker kill $(sudo docker ps | awk '{print $1}' | grep -v CONTAINER)
 
 # Remove stopped containers
-sudo docker container prune
+sudo docker container prune -f
 
 # Remove all volumes
 sudo docker volume rm $(sudo docker volume list | awk '{print $2}' | grep -v VOLUME)
+
+# Clean up mounted volumes and generated files
+sudo rm -rf services/timesketch/timesketch
+sudo rm -rf services/rocketchat/data
+sudo rm -f services/onlyoffice/docker-compose.yml
+sudo rm -rf services/onlyoffice/config/mysql/docker-entrypoint-initdb.d
+sudo rm -f services/timesketch/deploy_timesketch.sh
